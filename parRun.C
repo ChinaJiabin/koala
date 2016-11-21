2ee254cbed0b2935efa0d3f7dcdafd882933bea6
JiaBin-1 <489509928@qq.com>
2016-11-21 17:08:04 +0800

    Update parRun.C

--- parRun.C
+++ parRun.C
@@ -1,10 +1,10 @@
-#include <iostream>
 #include <string>
 #include <fstream>
 #include <iostream>
 namespace Koala
 {
 
+// Constructor
 parRun::parRun()
 {
   if ( ( path = getcwd(NULL, 0) ) == NULL )
@@ -12,5 +12,33 @@ parRun::parRun()
     std::cout<< "Error: getcwd()" << std::endl;
     exit(1);
   }
+}
+
+// IO
+std::string parRun::getFileFullPath
+(
+  const char* fileName   ,
+  const char* fileSuffix ,
+  const char* filePath
+) const
+{
+  std::string fileFullPath = path;
+
+  fileFullPath.append("/");
+  if (filePath)
+  {
+    fileFullPath.append("filePath");
+    fileFullPath.append("/");
+  }
+
+  if (fileName)
+  {
+    fileFullPath.append("fileName");
+    
+    if (fileSuffix)
+      fileFullPath.append(fileSuffix);
+  }
+
+  return fileFullPath;
 }
 }
