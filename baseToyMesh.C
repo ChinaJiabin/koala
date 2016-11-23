8fdbe2824681fdb4cf67c515689477c4d7719d0f
JiaBin-1 <489509928@qq.com>
2016-11-23 17:33:24 +0800

    Create baseToyMesh.C

---
+++ baseToyMesh.C
@@ -0,0 +1,14 @@
+namespace Koala
+{
+
+baseToyMesh::baseToyMesh
+(
+  const parRun& Run_         ,
+  const char* inputFileName_ ,
+  const char* filesPath_
+)
+:
+  Run(Run_)                     ,
+  inputFileName(inputFileName_) ,
+  filesPath(filesPath_) {}
+}
