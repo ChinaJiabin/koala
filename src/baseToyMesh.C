8f52684e236b7323bd46de659e905c41d5b92ddc
JiaBin-1 <489509928@qq.com>
2016-11-24 08:26:24 +0800

    Update baseToyMesh.C

--- src/baseToyMesh.C
+++ src/baseToyMesh.C
@@ -11,4 +11,13 @@ baseToyMesh::baseToyMesh
   Run(Run_)                     ,
   inputFileName(inputFileName_) ,
   filesPath(filesPath_) {}
+
+void baseToyMesh::writeBoundaryName() const
+{
+  std::ofstream file;
+  Run.openFile(file, "boundary", NULl, filesPath);
+
+  file << sizePatches << "\n";
+  file << patchesName;
+}
 }
