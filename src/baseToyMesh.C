15b8e1c9d886d9c67d20e512f8833315e1d2cfc9
JiaBin-1 <489509928@qq.com>
2016-11-30 10:11:02 +0800

    Update baseToyMesh.C

--- src/baseToyMesh.C
+++ src/baseToyMesh.C
@@ -20,4 +20,49 @@ void baseToyMesh::writeBoundaryName() const
   file << sizePatches << "\n";
   file << patchesName;
 }
+  
+void baseToyMesh::equalRatioDistribution1D
+(
+  double* map         ,
+  const int& size     ,
+  const double& ratio ,  
+  const int& type
+) const
+{
+}
+
+void baseToyMesh::lineMap1D
+(
+  const double* startPoint ,
+  const double* endPoint   ,
+  double* points           ,
+  const int& size          ,    
+  const int& dim           ,
+  const double& ratio      ,
+  const int& type
+) const
+{
+}
+
+void baseToyMesh::arcMap1D
+(
+  const double* startPoint ,
+  const double* endPoint   ,
+  double* points           ,
+  const int& size          ,    
+  const double& angle      ,
+  const double& ratio      ,
+  const int& type
+) const
+{
+}
+
+void baseToyMesh::givenPointsMap1D
+(
+  double* points            ,
+  const double* givenPoints ,
+  const int& size
+) const
+{
+}
 }
