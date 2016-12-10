8ea6c13fbce930c6a4ff823c9379ed0f342c149a
JiaBin-1 <489509928@qq.com>
2016-12-10 11:34:38 +0800

    Update test_fileIO.C

--- test/test_fileIO.C
+++ test/test_fileIO.C
@@ -1,7 +1,21 @@
 #include "parRun.H"
-using namespace Koala;
-
+using namespace Koala 
 int main(int argc, char* argv[])
 {
+  parRun Run;
+  
+  // File testInput only contains 1 
+  std::ifstream file;
+  Run.openfile(file, "testInput", NULL, NULL);
+  
+  int anIntBuffer;
+  file >> anIntBuffer;
+  if (anIntBuffer != 1)
+  {
+    std::cout<< "Test fileIO not pass!" << std::endl;
+    exit(1);
+  }
+  
+  std::cout<< "Test fileIO pass!" << std::endl;
   return 0;
 } 
