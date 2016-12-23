d99fdb6e9a4bb27098266df3be5cff6388459ba5
JiaBin-1 <489509928@qq.com>
2016-12-23 18:20:39 +0800

    1

--- test/test_toyMesh2D.C
+++ test/test_toyMesh2D.C
@@ -1,32 +1,33 @@
 #include "parRun.H"
 #include "toyMesh2D.H"
+#include "writeVTK.H"
 using namespace Koala;
 
 int main(int argc, char const *argv[])
 {
   parRun Run;
   toyMesh2D mesh(Run);
-  
+
   std::cout << "Write points" << std::endl;
   mesh.writePoints();
-  
+
   std::cout << "Write cells" << std::endl;
   mesh.writeCells();
-  
+
   std::cout << "Write faces" << std::endl;
   mesh.writeFaces();
-  
+
   std::cout << "Write boundary name" << std::endl;
-  mesh.writeBoundary();
-  
+  mesh.writeBoundaryName();
+
   std::cout << "Write boundary points id" << std::endl;
   mesh.writeBoundaryPointsId();
-  
+
   std::cout << "Write boundary faces" << std::endl;
-  mesh.writeBoubdaryFaces();
-  
+  mesh.writeBoundaryFaces();
+
   std::cout << "Convert to vtk format" << std::endl;
   writeToyMeshVTK(Run, 2);
-  
+
   return 0;
 }
